36dc5426bc83433c26d9156524e90e8cacf661e6
guinotix <al375927@uji.es>
2023-03-08 10:52:00 +0100

    part 1: create interface and show the object

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from '../interfaces/hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
 })
 export class HeroesComponent {
 
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
 }
